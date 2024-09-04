59d80d271d912ee07b37c0e6c98c39c73c953baa
Kevin Ashworth <kevinashworth@yahoo.com>
2024-09-04 09:54:22 -0700

    Add some useful packages
    vitest, shadcn, types

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,17 @@
 import { spectrum } from "./src/theme";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: spectrum,
-    },
+  	extend: {
+  		colors: {},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
